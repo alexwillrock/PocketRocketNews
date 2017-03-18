9128c6f62913f178464dbb506a30b161608cf0ae
alexwillrock <whitewillrock@gmail.com>
2017-03-18 13:38:33 +0700

    Feed list View init

--- PocketRocketNews/Classes/UserStory/FeedStory/FeedListModule/View/FeedListViewController.swift
+++ PocketRocketNews/Classes/UserStory/FeedStory/FeedListModule/View/FeedListViewController.swift
@@ -17,6 +17,8 @@ final class FeedListViewController: BaseViewController {
     
     //MARK: private
     
+    fileprivate let kHeight: CGFloat = 120.0
+    
     fileprivate var controller: FeedListController {
         var _controller = FeedListController()
         _controller.delegate = self;
@@ -26,8 +28,38 @@ final class FeedListViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(cellType: FeedListCell.self)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = kHeight
         // Do any additional setup after loading the view.
     }
+}
+
+extension FeedListViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: FeedListCell.self)
+     
+        return cell
+    }
+}
+
+extension FeedListViewController: UITableViewDelegate{
+    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return kHeight
+//    }
+    
 }
 
 extension FeedListViewController: FeedListControllerDelegate{
---
+++ PocketRocketNews/Classes/UserStory/FeedStory/Storyboard/Cells/FeedListCell/FeedListCell.swift
@@ -0,0 +1,21 @@
+//
+//  FeedListCell.swift
+//  PocketRocketNews
+//
+//  Created by Алексей on 18.03.17.
+//  Copyright © 2017 Алексей. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+class FeedListCell: UITableViewCell, NibReusable {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = UIColor.clear
+        // Initialization code
+    }
+    
+}
